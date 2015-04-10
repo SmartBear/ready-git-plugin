6dc4a03e05416991ef1f7549e5a48f8bebbdc8fe
anders.jaensson <anders.jaensson@smartbear.com>
2015-04-10 16:54:17 +0200

    API-1145: Fixed compilation error in test

--- src/test/java/com/smartbear/readyapi/plugin/git/ui/GitRepositorySelectionGuiTest.java
+++ src/test/java/com/smartbear/readyapi/plugin/git/ui/GitRepositorySelectionGuiTest.java
@@ -1,6 +1,6 @@
 package com.smartbear.readyapi.plugin.git.ui;
 
-import com.eviware.soapui.model.project.Project;
+import com.eviware.soapui.impl.wsdl.WsdlProject;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -17,8 +17,8 @@ public class GitRepositorySelectionGuiTest extends SwingTest {
     @Test
     @Ignore("This is a manual test")
     public void showDialog() throws Exception {
-        Project project = Mockito.mock(Project.class);
+        WsdlProject project = Mockito.mock(WsdlProject.class);
         when(project.getPath()).thenReturn("/tmp/apan");
-        embedInFrameAndShow(new GitRepositorySelectionGui(project).getComponent());
+        embedInFrameAndShow(new GitRepositorySelectionGui(project, null).getComponent());
     }
 }
