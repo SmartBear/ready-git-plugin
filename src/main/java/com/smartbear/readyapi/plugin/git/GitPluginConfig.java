f67c291ef5fe4e022d63ade0f3db8a9196eb1bac
Prakash Jat <prakash.jat@smartbear.com>
2015-04-20 14:16:06 +0200

    Minimum R!A version should be 1.3

--- src/main/java/com/smartbear/readyapi/plugin/git/GitPluginConfig.java
+++ src/main/java/com/smartbear/readyapi/plugin/git/GitPluginConfig.java
@@ -3,9 +3,10 @@ package com.smartbear.readyapi.plugin.git;
 import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 
-@PluginConfiguration(groupId = "com.smartbear.readyapi.plugins", name ="Ready! API Git Plugin",
-version = "0.1", autoDetect = true,
-description = "A git plugin to share composite projects", infoUrl = "https://github.com/SmartBear/ready-git-plugin")
+@PluginConfiguration(groupId = "com.smartbear.readyapi.plugins", name = "Ready! API Git Plugin",
+        version = "0.1", autoDetect = true,
+        description = "A git plugin to share composite projects", infoUrl = "https://github.com/SmartBear/ready-git-plugin",
+        minimumReadyApiVersion = "1.3.0")
 public class GitPluginConfig extends PluginAdapter {
 
 }
