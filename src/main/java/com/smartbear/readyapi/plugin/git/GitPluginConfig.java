eba50421036c00f2fc2e90da27fd88e5584e05eb
AQA\Alexander.Nikitin <Alexander.Nikitin@smartbear.com>
2017-11-15 15:10:43 +0300

    RIA-3978 Depends on ReadyAPI 2.3.0

--- src/main/java/com/smartbear/readyapi/plugin/git/GitPluginConfig.java
+++ src/main/java/com/smartbear/readyapi/plugin/git/GitPluginConfig.java
@@ -6,7 +6,7 @@ import com.eviware.soapui.plugins.PluginConfiguration;
 @PluginConfiguration(groupId = "com.smartbear.readyapi.plugins", name = "Git Integration Plugin",
         version = "1.1.0-SNAPSHOT", autoDetect = true,
         description = "A git plugin to share composite projects", infoUrl = "https://github.com/SmartBear/ready-git-plugin",
-        minimumReadyApiVersion = "1.3.0")
+        minimumReadyApiVersion = "2.3.0")
 public class GitPluginConfig extends PluginAdapter {
 
 }
